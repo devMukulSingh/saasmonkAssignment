9f2ea3505354d6ef5c8b037be1d24fe89d88bee0
devMukulSingh <mukulsingh2276@gmail.com>
2024-10-01 22:44:53 +0530

    error fixes

---
+++ app/(root)/(home)/components/HomePage.tsx
@@ -0,0 +1,19 @@
+"use client";
+import SearchBar from "./SearchBar";
+import MovieCards from "./MovieCards";
+import { useSearchParams } from "next/navigation";
+import SearchMovies from "./SearchMovies";
+import { Fragment, Suspense } from "react";
+
+const HomePage = () => {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query");
+  return (
+    <Fragment>
+      <SearchBar />
+        {query ? <SearchMovies query={query} /> : <MovieCards />}
+    </Fragment>
+  );
+};
+
+export default HomePage;
--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,19 +1,13 @@
-"use client";
-import SearchBar from "./components/SearchBar";
-import MovieCards from "./components/MovieCards";
-import { useSearchParams } from "next/navigation";
-import SearchMovies from "./components/SearchMovies";
 import { Suspense } from "react";
+import HomePage from "./components/HomePage";
 
 export default function Home() {
-  const searchParams = useSearchParams();
-  const query = searchParams.get("query");
+
   return (
     <main className="flex flex-col gap-8 p-5">
       <h1 className="text-3xl">The best movie reviews site!</h1>
-      <SearchBar />
       <Suspense>
-        {query ? <SearchMovies query={query} /> : <MovieCards />}
+        <HomePage/>
       </Suspense>
     </main>
   );
